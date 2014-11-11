f872ce998d23cc76133a633a315311c39d12fa27
dennisdembo <dennis.olsson@mail.com>
2014-11-11 15:33:29 +0100

    Create ExecuteProgram.java

---
+++ Uppgift-3/ExecuteProgram.java
@@ -0,0 +1,8 @@
+import java.sql.SQLException;
+import View.GUI;
+
+public class ExecuteProgram {	
+	public static void main(String[] args) throws SQLException {
+		new GUI().mainFrame.setVisible(true);
+	}
+}
